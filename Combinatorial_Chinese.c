2794304d2d36c3337c44b41665fb585394eebd06
DingCao <349373001@qq.com>
2014-11-01 01:00:41 +0800

    push a version to solve combinatorial

---
+++ Combinatorial_Chinese.c
@@ -0,0 +1,43 @@
+// Copyright (c) 2014 Junjie_Huang@SYSU(SNO:13331087). All Rights Reserved.
+// 求组合数
+
+#include "stdio.h"
+
+#define ERROR -1
+
+// 求一个非负整数的阶乘
+long int factorial(int n) {
+  long int result = 1;
+  int i;
+  for (i = 1; i <= n; i++) result *= i;  // 从1乘到n
+
+  return result;
+}
+
+// 求从n个数中取m个数所能产生的组合数。
+long int Combinatorial(int m, int n) {
+  long int numerator;    // 分子
+  long int denominator;  // 分母
+
+  // 如果n为零或者m比n大，或者m、n为负数，这些都是错误的情况，返回一个错误标示
+  if (m > n || n <= 0 || m < 0) return ERROR;
+
+  // 其他情况计算结果。
+  numerator = factorial(n);
+  denominator = factorial(n - m) * factorial(m);
+
+  return numerator / denominator;
+}
+
+int main() {
+  int test_cases;  // 测试样例数
+
+  // for(初始条件; 判断条件; 尾执行语句) {...}
+  for (scanf("%d", &test_cases); test_cases; test_cases--) {
+    long int m, n;
+    scanf("%ld %ld", &m, &n);
+    printf( "%ld\n", Combinatorial(m, n));
+  }
+
+  return 0;
+}
