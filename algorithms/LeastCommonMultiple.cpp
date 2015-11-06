60922160c9d4b7ff62af874539cb7b244d9824a1
DingCao <349373001@qq.com>
2015-11-06 15:36:53 +0800

    a small optimization of LCM

--- algorithms/LeastCommonMultiple.cpp
+++ algorithms/LeastCommonMultiple.cpp
@@ -4,9 +4,10 @@
 // @Param m, n: the 2 numbers the LCM of which is to be found.
 int LCM(int m, int n) {
   int multiple = MAX(m, n);       // get the bigger
-  for (; multiple; multiple++) {
+  while (true) {
     // the first multiple met is the LCM of m, n
     if (multiple%m == 0 && multiple%n == 0) break;
+    multiple++;
   }
 
   return multiple;
